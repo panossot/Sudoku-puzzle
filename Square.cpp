3acabae73c3cbabe916ad24ded1b10d6c0960e3d
Artyom Pavlichenko <KomIG@wirelessprvnat-172-16-195-205.near.illinois.edu>
2013-10-25 16:52:52 -0500

    Changes values

--- Square.cpp
+++ Square.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 class Square{ 
-	bool contains;
-	int num;
+	bool shown;
+	int val;
 	public:
 		Square (bool, int);
-		Square () {} 
-		void chCont() {contains = !contains;}
-		bool getCont() {return contains;}
-		int getNum() {return num;}
-		void setNum(int val) {num = val;}
+		Square () {contains = true; num =0;} 
+		void chShwn() {shown = !shown;}
+		bool getShwn() {return shown;}
+		int getVal() {return val;}
+		void setVal(int val) {val = val;}
 };
 
 Square::Square(bool val,int a) {
@@ -20,7 +20,7 @@ Square::Square(bool val,int a) {
 int main() {
 	Square one (true,1);
 	Square two (true,2);
-	Square ray[9][9](true, 0);
+	Square ray[9][9];
 	for(int r=0;r<9;r++) {
 		for(int c=0;c<9;c++) {
 			ray[r][c].setNum(c);
@@ -28,12 +28,10 @@ int main() {
 	}
 	for(int r=0;r<9;r++) {
 		for(int c=0;c<9;c++) {
-			std::cout << ray[r][c].getNum() << std::endl;	
+			std::cout << ray[r][c].getNum();	
 		}
+		std::cout << std::endl;
 	}
-
-	//std::cout << "one: " << one.getCont() << std::endl;
-	//std::cout << "two: " << two.getCont() << std::endl;
 	return 0;
 }
 		 
